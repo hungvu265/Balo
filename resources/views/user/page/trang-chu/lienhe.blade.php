502b9b7434eb9f7efda3100ef846bf174b0e35a9
hungvu <hung.vu2@ntq-solution.com.vn>
2023-12-30 13:32:58 +0700

    add: code base

---
+++ resources/views/user/page/trang-chu/lienhe.blade.php
@@ -0,0 +1,80 @@
+@extends('user.master')
+@section('content')
+
+
+    <section class="bread-crumb clearfix">
+<span class="crumb-border"></span>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 a-left">
+            <ul class="breadcrumb">					
+                <li class="home">
+                    <a itemprop="url" href="trangchu" ><span >Trang chủ</span></a>						
+                    <span class="mr_lr"> <i class="fa fa-angle-right"></i> </span>
+                </li>
+                    
+                <li >
+                    <a itemprop="url" href="lienhe"><span >Thông Tin Liên Hệ</span></a>	
+                    <span class="mr_lr"> <i class="fa fa-angle-right"></i> </span>
+                </li>
+
+                <li >
+                    <h2 style="color: #0a0475;text-align: left" class="vc_custom_heading">LIÊN HỆ VỚI <p></p></h2>             
+                </li>
+
+                <li >
+
+                
+                <div id="fb-root"></div>
+                <script async defer crossorigin="anonymous" src="#" nonce="UfF4nwNS"></script>
+                <div style="margin-left: 650px;" class="fb-page" data-href="#" data-tabs="timeline" data-width="500" data-height="400" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/tuyenphung0912" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/tuyenphung0912/">Shop </a></blockquote></div>
+                 
+                    
+                    <ul>
+                        <li>
+                            <a href="trangchu" class="logo-wrapper ">
+                                <img src="#" alt="logo ">
+                            </a><br><br>
+                        </li>           
+                        <li>
+                            <span class="fa fa-home"></span> <span class="frist_"><span class="b_font"></span>số 1, Đại Cồ Việt, Hai Ba Trưng, Hà Nội, Việt Nam</span><br><br>
+                        </li>
+
+                        <li>
+                            <span class="fa fa-phone"></span><span class="frist_"><span class="b_font"><a href="tel:0328730391"> 0328730391</a></span></span><br><br>
+                        </li>
+
+                        <li>
+                        <span class="fa fa-paper-plane"></span><span class="frist_"><span class="b_font"><a href="mailto:"> Admin@gmail.com</a> </span><br></span><br>
+                        <li>
+            
+                        <li>                
+                            <div class="social_right">
+                                <a href="#">
+                                    <i class="fa fa-facebook"></i>
+                                </a>   
+                                <a href="#"><i class="fa fa-youtube"></i></a>
+                            </div>
+                        <li>
+                    </ul>
+                </li>
+                  
+                <li>
+                    <!-- Map -->
+                    <div class="wpb_gmaps_widget wpb_content_element">
+                        <div class="wpb_wrapper">
+                            <div class="wpb_map_wraper">
+                            <iframe style="margin-top: 200px;" src="https://www.google.com/maps/place/1+%C4%90%E1%BA%A1i+C%E1%BB%93+Vi%E1%BB%87t,+B%C3%A1ch+Khoa,+Thanh+Xu%C3%A2n,+H%C3%A0+N%E1%BB%99i,+Vi%E1%BB%87t+Nam/@21.0040085,105.8401542,17z/data=!3m1!4b1!4m6!3m5!1s0x3135ad2d22966207:0xc027ec67cf6a7872!8m2!3d21.0040035!4d105.8427291!16s%2Fg%2F11vdsb2pgj?hl=vi-VN&entry=ttu" width="1150" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>		</div>
+                            </div>
+                        </div>    
+                    </div>
+                <li>
+            </ul>
+    </div>
+</div>
+
+
+
+</body>
+</html>
+@endsection	
